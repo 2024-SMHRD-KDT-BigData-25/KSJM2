88c8fce1747fcf3048a5dd59f3225858fca4ed36
Junhyeon Moon <wizd.mooncre@gmail.com>
2024-10-10 10:20:10 +0900

    문준현 - 수정

--- Pot/src/main/java/com/smhrd/controller/SnsWrite.java
+++ Pot/src/main/java/com/smhrd/controller/SnsWrite.java
@@ -17,23 +17,20 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
 
-import com.smhrd.model.PotSale;
 import com.smhrd.model.PotSns;
-import com.smhrd.model.SaleDAO;
 import com.smhrd.model.SnsDAO;
-
 
 @WebServlet("/jsp/SnsWrite")
 public class SnsWrite extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	 // 업로드 설정: 메모리 상에서 저장할 파일 사이즈와 임시 저장 위치
+    // 업로드 설정: 메모리 상에서 저장할 파일 사이즈와 임시 저장 위치
     private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3; // 3MB
     private static final int MAX_FILE_SIZE = 1024 * 1024 * 40; // 40MB
     private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50; // 50MB
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 // 파일이 첨부된 multipart 요청인지 확인
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 파일이 첨부된 multipart 요청인지 확인
         if (!ServletFileUpload.isMultipartContent(request)) {
             // 폼이 multipart/form-data가 아니면 오류 처리
             request.setAttribute("message", "Error: Form must have enctype=multipart/form-data.");
@@ -52,16 +49,14 @@ public class SnsWrite extends HttpServlet {
         }
 
         // 업로드된 파일 이름을 저장할 리스트
-        List<String> imgList = new ArrayList();
+        List<String> imgList = new ArrayList<>();
 
         // Apache Commons FileUpload 설정
         DiskFileItemFactory factory = new DiskFileItemFactory();
-        // 메모리 임계값을 초과하면 임시 디렉토리에 파일을 저장
         factory.setSizeThreshold(MEMORY_THRESHOLD);
         factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
 
         ServletFileUpload upload = new ServletFileUpload(factory);
-        // 업로드 파일 크기 설정
         upload.setFileSizeMax(MAX_FILE_SIZE);
         upload.setSizeMax(MAX_REQUEST_SIZE);
 
@@ -80,12 +75,11 @@ public class SnsWrite extends HttpServlet {
                     // form 필드일 경우 (텍스트 필드 처리)
                     String fieldName = item.getFieldName();
                     String fieldValue = item.getString("UTF-8"); // UTF-8로 인코딩 처리
-                    System.out.println("Field Name: " + fieldName + ", Field Value: " + fieldValue);
                     
                     switch (fieldName) {
-                    	case "user_id":
-                    		user_id = fieldValue;
-                    		break;
+                        case "user_id":
+                            user_id = fieldValue;
+                            break;
                         case "sns_title":
                             sns_title = fieldValue;
                             break;
@@ -95,30 +89,31 @@ public class SnsWrite extends HttpServlet {
                     }
                 } else {
                     // 파일일 경우 (업로드 파일 처리)
-                    String fileName = new File(item.getName()).getName();
-                    String filePath = uploadPath + File.separator + fileName;
-                    File storeFile = new File(filePath);
+                    if (item.getSize() > 0) { // 파일이 선택된 경우
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        File storeFile = new File(filePath);
 
-                    // 파일이름 중복 처리 (같은 이름 파일이 존재하면 덮어쓰지 않고 새로운 이름 생성)
-                    String newFileName = FilenameUtils.getBaseName(fileName) + "_" + System.currentTimeMillis() + "." + FilenameUtils.getExtension(fileName);
-                    File renamedFile = new File(uploadPath + File.separator + newFileName);
+                        // 파일이름 중복 처리
+                        String newFileName = FilenameUtils.getBaseName(fileName) + "_" + System.currentTimeMillis() + "." + FilenameUtils.getExtension(fileName);
+                        File renamedFile = new File(uploadPath + File.separator + newFileName);
 
-                    // 파일 저장
-                    item.write(renamedFile);
-                    imgList.add(newFileName); // 저장된 파일 이름을 리스트에 추가
-                    System.out.println("Uploaded File Name: " + newFileName);
+                        // 파일 저장
+                        item.write(renamedFile);
+                        imgList.add(newFileName); // 저장된 파일 이름을 리스트에 추가
+                        System.out.println("Uploaded File Name: " + newFileName);
+                    }
                 }
             }
 
-            // 업로드된 이미지들을 쉼표로 연결
-            String sns_img = String.join(",", imgList);
+            // 업로드된 이미지들을 쉼표로 연결, 선택된 파일이 없으면 sns_img는 null로 설정
+            String sns_img = imgList.isEmpty() ? null : String.join(",", imgList);
             System.out.println("Uploaded Images: " + sns_img);
 
-            // PotSale 객체 생성 및 데이터베이스 저장
+            // PotSns 객체 생성 및 데이터베이스 저장
             PotSns snswrite = new PotSns(user_id, sns_title, sns_img, sns_content);
-            
             SnsDAO dao = new SnsDAO();
-    		int res = dao.writeSns(snswrite);
+            int res = dao.writeSns(snswrite);
             
             // 결과에 따라 리다이렉트 처리
             if (res > 0) {
@@ -132,6 +127,5 @@ public class SnsWrite extends HttpServlet {
             request.setAttribute("message", "There was an error: " + ex.getMessage());
             response.sendRedirect("sns_write.jsp");
         }
-	}
-
+    }
 }
